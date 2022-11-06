602686423873acfb398a784f462f8777d70aace0
Milton <milton.dev.sis@gmail.com>
2022-11-06 08:06:23 -0300

    Project done

--- DeepLinkGuide/app/src/main/java/com/mpfcoding/deeplink_guide/MainActivity.kt
+++ DeepLinkGuide/app/src/main/java/com/mpfcoding/deeplink_guide/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.mpfcoding.deeplink_guide
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.mpfcoding.deeplink_guide.ui.theme.DeepLink_GuideTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +30,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DeepLink_GuideTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Button(onClick = {
+                                navController.navigate("detail")
+                            }) {
+                                Text(text = "To detail")
+                            }
+                        }
+                    }
+                    composable(
+                        route = "detail",
+                        deepLinks = listOf(
+                            navDeepLink {
+                                uriPattern = "https://pl-coding.com/{id}"
+                                action = Intent.ACTION_VIEW
+                            }
+                        ),
+                        arguments = listOf(
+                            navArgument("id") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) { entry ->
+                        val id = entry.arguments?.getInt("id")
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "The id is $id")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DeepLink_GuideTheme {
-        Greeting("Android")
     }
 }
